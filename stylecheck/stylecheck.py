728943d74465c99aa3b50170468f27304e072a9e
Markus Svensson <markus.sve@gmail.com>
2017-07-20 21:41:23 +0200

    Refactored to add init function to make it easier to test.

--- stylecheck/stylecheck.py
+++ stylecheck/stylecheck.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+import sys
 
 
 def main():
     print("hello world")
 
 
-if __name__ == '__main__':
-    main()
+def init():
+    if __name__ == '__main__':
+        sys.exit(main())
+
+init()
